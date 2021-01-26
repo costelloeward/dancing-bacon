95e73fb6efa10ca7d25d8f2697888febf02e3a35
LouiseCW <costelloeward@gmail.com>
2021-01-26 18:38:09 +0000

    connect stylesheet in static folder

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
 res.sendFile(__dirname + "/index.html");
@@ -9,4 +10,8 @@ res.sendFile(__dirname + "/index.html");
 
 app.post("/", function(req,res){
   console.log(req.body.bacon);
+});
+
+app.listen(3000,function(){
+  console.log("Server running on port 3000");
 });
