e89e29c3cb4dfb6601a4d098641723394112a7c8
LouiseCW <costelloeward@gmail.com>
2021-02-12 18:17:54 +0000

    change ot CSS grid

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ res.sendFile(__dirname + "/index.html");
 app.post("/", function(req,res){
   console.log(req.body.bacon);
 });
+app.get("/yes",function(req,res){
+res.sendFile(__dirname + "/yes.html");
+});
+
+app.get("/no",function(req,res){
+res.sendFile(__dirname + "/no.html");
+});
 
 app.listen(3000,function(){
   console.log("Server running on port 3000");
