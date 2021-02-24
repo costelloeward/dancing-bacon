dce32683b570ed1434fe377a68e470acfee8e349
LouiseCW <costelloeward@gmail.com>
2021-02-24 19:37:14 +0000

    add if statement to form rendering

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-res.sendFile(__dirname + "/index.html");
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req,res){
+app.post("/", function(req, res) {
   console.log(req.body.bacon);
-});
-app.get("/yes",function(req,res){
-res.sendFile(__dirname + "/yes.html");
+  const answer = req.body.bacon;
+  if (answer === "yes") {
+    app.get("/yes", function(req, res) {
+      res.sendFile(__dirname + "/yes.html");
+    });
+  } else if (answer === "no") {
+    app.get("/no", function(req, res) {
+      res.sendFile(__dirname + "/no.html");
+    });
+  } else {
+    app.get("/other", function(req, res) {
+      res.sendFile(__dirname + "/other.html");
+    });
+  }
 });
 
-app.get("/no",function(req,res){
-res.sendFile(__dirname + "/no.html");
-});
-
-app.listen(3000,function(){
+app.listen(3000, function() {
   console.log("Server running on port 3000");
 });
